bef37501bc4bf8cb016126b6a3356fb7dbc9474b
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-07-04 12:34:54 +0300

    Refactoring buttom

--- src/main/java/com/team1/currencyconverter/service/TelegramBot.java
+++ src/main/java/com/team1/currencyconverter/service/TelegramBot.java
@@ -12,7 +12,6 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.util.*;
 
