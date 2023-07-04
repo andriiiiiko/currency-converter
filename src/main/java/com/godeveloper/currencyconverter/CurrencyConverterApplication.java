021e399f60479191bcf888c99eaa0c2d91757cb0
Andrii Protas <andreyprotas5@gmail.com>
2023-07-04 19:10:24 +0200

    Renamed package

--- src/main/java/com/godeveloper/currencyconverter/CurrencyConverterApplication.java
+++ src/main/java/com/godeveloper/currencyconverter/CurrencyConverterApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyconverterApplication {
+public class CurrencyConverterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyconverterApplication.class, args);
+		SpringApplication.run(CurrencyConverterApplication.class, args);
 	}
 }
