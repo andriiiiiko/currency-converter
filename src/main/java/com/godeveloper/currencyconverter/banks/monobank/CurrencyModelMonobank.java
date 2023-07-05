d29bc5a529ec57c7cb90d80a6737fdb91e083582
Serhii <serhii.mask@gmail.com>
2023-07-05 18:30:06 +0300

    fix: fixed models classes

--- src/main/java/com/godeveloper/currencyconverter/banks/monobank/CurrencyModelMonobank.java
+++ src/main/java/com/godeveloper/currencyconverter/banks/monobank/CurrencyModelMonobank.java
@@ -1,5 +1,8 @@
 package com.godeveloper.currencyconverter.banks.monobank;
 
+import lombok.Data;
+
+@Data
 public class CurrencyModelMonobank {
 
     private final int currencyCodeA;
@@ -8,49 +11,4 @@ public class CurrencyModelMonobank {
     private final float rateBuy;
     private final float rateCross;
     private final float rateSell ;
-
-    public CurrencyModelMonobank(int currencyCodeA, int currencyCodeB, int date, float rateBuy, float rateCross, float rateSell) {
-        this.currencyCodeA = currencyCodeA;
-        this.currencyCodeB = currencyCodeB;
-        this.date = date;
-        this.rateBuy = rateBuy;
-        this.rateCross = rateCross;
-        this.rateSell = rateSell;
-    }
-
-    public int getCurrencyCodeA() {
-        return currencyCodeA;
-    }
-
-    public int getCurrencyCodeB() {
-        return currencyCodeB;
-    }
-
-    public int getDate() {
-        return date;
-    }
-
-    public float getRateBuy() {
-        return rateBuy;
-    }
-
-    public float getRateCross() {
-        return rateCross;
-    }
-
-    public float getRateSell() {
-        return rateSell;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                "currencyCodeA=" + currencyCodeA +
-                ", currencyCodeB=" + currencyCodeB +
-                ", date=" + date +
-                ", rateBuy=" + rateBuy +
-                ", rateCross=" + rateCross +
-                ", rateSell=" + rateSell +
-                '}';
-    }
 }
