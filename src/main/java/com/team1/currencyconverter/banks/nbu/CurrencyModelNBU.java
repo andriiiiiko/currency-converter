c59c27a85f5e38a4cd3113a3aca062a40385686f
Serhii <serhii.mask@gmail.com>
2023-07-05 12:03:53 +0300

    feat: add API CurrencyServiceNBU

---
+++ src/main/java/com/team1/currencyconverter/banks/nbu/CurrencyModelNBU.java
@@ -0,0 +1,49 @@
+package com.team1.currencyconverter.banks.nbu;
+
+public class CurrencyModelNBU {
+
+    private int r030;
+    private String txt;
+    private float rate;
+    private String cc;
+    private String exchangedate;
+
+    public CurrencyModelNBU(int r030, String txt, float rate, String cc, String exchangedate) {
+        this.r030 = r030;
+        this.txt = txt;
+        this.rate = rate;
+        this.cc = cc;
+        this.exchangedate = exchangedate;
+    }
+
+    public int getR030() {
+        return r030;
+    }
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public String getExchangedate() {
+        return exchangedate;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "r030=" + r030 +
+                ", txt='" + txt + '\'' +
+                ", rate=" + rate +
+                ", cc='" + cc + '\'' +
+                ", exchangedate='" + exchangedate + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/team1/currencyconverter/banks/nbu/CurrencyServiceNBU.java
@@ -0,0 +1,55 @@
+package com.team1.currencyconverter.banks.nbu;
+
+import com.google.gson.Gson;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class CurrencyServiceNBU {
+
+    private static final String BASE_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+    private static final Gson GSON = new Gson();
+    private static final HttpClient HTTP_CLIENT = HttpClients.createDefault();
+
+    public static List<CurrencyModelNBU> getCurrencyRate() {
+        HttpGet request = new HttpGet(BASE_URL);
+
+        try {
+            HttpResponse response = HTTP_CLIENT.execute(request);
+            int statusCode = response.getStatusLine().getStatusCode();
+
+            if (statusCode == HttpStatus.SC_OK) {
+                String responseBody = EntityUtils.toString(response.getEntity());
+                CurrencyModelNBU[] tasks = GSON.fromJson(responseBody, CurrencyModelNBU[].class);
+
+                return Arrays.asList(tasks);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String getCurrencyInformation(String currency) {
+        List<CurrencyModelNBU> currencyList = getCurrencyRate();
+
+        if (currencyList != null) {
+            for (CurrencyModelNBU currencyModelNBU : currencyList) {
+                if (currencyModelNBU.getCc().equals(currency)) {
+                    return "Курси в NBU: " + currency + "/UAN" + "\n" +
+                            "Купівля: " + currencyModelNBU.getRate() + "\n" +
+                            "Продаж: " + currencyModelNBU.getRate();
+                }
+            }
+        }
+
+        return null;
+    }
+}
