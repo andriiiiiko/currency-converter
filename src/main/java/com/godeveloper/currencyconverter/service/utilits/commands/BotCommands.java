5cc2f077d24af0639ee5baf512d65e65ba00d2ae
dmytrovolodovskyi <d.volodovskyi@gmail.com>
2023-07-12 23:36:18 +0200

    Added functionality to the button

--- src/main/java/com/godeveloper/currencyconverter/service/utilits/commands/BotCommands.java
+++ src/main/java/com/godeveloper/currencyconverter/service/utilits/commands/BotCommands.java
@@ -85,16 +85,19 @@ public class BotCommands {
     public void setTwoNumbers(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setNumber("2");
+        numberSettings(chatId);
     }
 
     public void setThreeNumbers(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setNumber("3");
+        numberSettings(chatId);
     }
 
     public void setFourNumbers(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setNumber("4");
+        numberSettings(chatId);
     }
 
     public void currencySettings(long chatId) {
@@ -115,11 +118,13 @@ public class BotCommands {
     public void setUSD(long chatId) {
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setCurrency("USD");
+        currencySettings(chatId);
     }
 
     public void setEUR(long chatId) {
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setCurrency("EUR");
+        currencySettings(chatId);
     }
 
     public void bankSettings(long chatId) {
@@ -141,16 +146,19 @@ public class BotCommands {
     public void setMono(long chatId) {
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setBank("Моно");
+        bankSettings(chatId);
     }
 
     public void setPrivat(long chatId) {
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setBank("Приват");
+        bankSettings(chatId);
     }
 
     public void setNBU(long chatId) {
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setBank("НБУ");
+        bankSettings(chatId);
     }
 
     public void timeSettings(long chatId) {
@@ -181,55 +189,66 @@ public class BotCommands {
     public void setTime9(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("09:00");
+        timeSettings(chatId);
     }
 
     public void setTime10(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("10:00");
+        timeSettings(chatId);
     }
 
     public void setTime11(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("11:00");
+        timeSettings(chatId);
     }
 
     public void setTime12(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("12:00");
+        timeSettings(chatId);
     }
 
     public void setTime13(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("13:00");
+        timeSettings(chatId);
     }
 
     public void setTime14(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("14:00");
+        timeSettings(chatId);
     }
 
     public void setTime15(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("15:00");
+        timeSettings(chatId);
     }
 
     public void setTime16(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("16:00");
+        timeSettings(chatId);
     }
 
     public void setTime17(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("17:00");
+        timeSettings(chatId);
     }
 
     public void setTime18(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("18:00");
+        timeSettings(chatId);
     }
 
     public void setTimeOff(long chatId){
         sendMessage.setChatId(chatId);
         getUserSettingsById(chatId).setTime("Вимкнути сповіщення");
+        timeSettings(chatId);
     }
 }
