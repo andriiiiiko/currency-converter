df3dee645cef8ab68d187fe8ee0ec40517282278
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-07-13 13:58:59 +0300

    update

--- src/main/java/com/godeveloper/currencyconverter/service/utilits/Buttons.java
+++ src/main/java/com/godeveloper/currencyconverter/service/utilits/Buttons.java
@@ -7,7 +7,8 @@ import static com.godeveloper.currencyconverter.service.utilits.ui.UserServices.
 public class Buttons {
 
     public static InlineKeyboardMarkup time(long chatId) {
-        InlineKeyboardMarkup markup = InlineKeyboardMarkupBuilder.buildMarkup(
+
+        return InlineKeyboardMarkupBuilder.buildMarkup(
                 new String[]{
                         getUserSettingsById(chatId).getTime().equals("09:00") ? "✅ 09:00" : "09:00",
                         getUserSettingsById(chatId).getTime().equals("10:00") ? "✅ 10:00" : "10:00",
@@ -23,66 +24,58 @@ public class Buttons {
                                 "✅ Вимкнути сповіщення" : "Вимкнути сповіщення",
                         "\uD83D\uDD19 Назад"
                 });
-
-        return markup;
     }
 
     public static InlineKeyboardMarkup bank(long chatId) {
-        InlineKeyboardMarkup markup = InlineKeyboardMarkupBuilder.buildMarkup(
+
+        return InlineKeyboardMarkupBuilder.buildMarkup(
                 new String[]{
                         getUserSettingsById(chatId).getBank().equals("НБУ") ? "✅ НБУ" : "НБУ",
                         getUserSettingsById(chatId).getBank().equals("Приват") ? "✅ Приват" : "Приват",
                         getUserSettingsById(chatId).getBank().equals("Моно") ? "✅ Моно" : "Моно",
                         "\uD83D\uDD19 Назад"
                 });
-
-        return markup;
     }
 
     public static InlineKeyboardMarkup currency(long chatId) {
-        InlineKeyboardMarkup markup = InlineKeyboardMarkupBuilder.buildMarkup(
+
+        return InlineKeyboardMarkupBuilder.buildMarkup(
                 new String[]{
                         getUserSettingsById(chatId).getCurrency().equals("USD") ? "✅ USD" : "USD",
                         getUserSettingsById(chatId).getCurrency().equals("EUR") ? "✅ EUR" : "EUR",
                         "\uD83D\uDD19 Назад"
                 });
-
-        return markup;
     }
 
     public static InlineKeyboardMarkup number(long chatId) {
-        InlineKeyboardMarkup markup = InlineKeyboardMarkupBuilder.buildMarkup(
+
+        return InlineKeyboardMarkupBuilder.buildMarkup(
                 new String[]{
                         getUserSettingsById(chatId).getNumber().equals("2") ? "✅ 2" : "2",
                         getUserSettingsById(chatId).getNumber().equals("3") ? "✅ 3" : "3",
                         getUserSettingsById(chatId).getNumber().equals("4") ? "✅ 4" : "4",
                         "\uD83D\uDD19 Назад"
                 });
-
-        return markup;
     }
 
-    public static InlineKeyboardMarkup setting(long chatId) {
-        InlineKeyboardMarkup markup = InlineKeyboardMarkupBuilder.buildMarkup(
+    public static InlineKeyboardMarkup setting() {
+
+        return InlineKeyboardMarkupBuilder.buildMarkup(
                 new String[]{
                         "\uD83C\uDFE6 Банк", "\uD83D\uDCB5 Валюта", "\uD83D\uDD52 Час сповіщень",
                         "\uD83D\uDD22 Кількість знаків після коми", "\uD83C\uDFE0 На головну"
                 });
-
-        return markup;
     }
 
-    public static InlineKeyboardMarkup info(long chatId) {
-        InlineKeyboardMarkup markup = InlineKeyboardMarkupBuilder.buildMarkup(
+    public static InlineKeyboardMarkup info() {
+
+        return InlineKeyboardMarkupBuilder.buildMarkup(
                 new String[]{"⚙ Налаштування"});
-
-        return markup;
     }
 
-    public static InlineKeyboardMarkup start(long chatId) {
-        InlineKeyboardMarkup markup = InlineKeyboardMarkupBuilder.buildMarkup(
-                new String[]{"\uD83D\uDCB1 Отримати інфо", "⚙ Налаштування"});
+    public static InlineKeyboardMarkup start() {
 
-        return markup;
+        return InlineKeyboardMarkupBuilder.buildMarkup(
+                new String[]{"\uD83D\uDCB1 Отримати інфо", "⚙ Налаштування"});
     }
 }
